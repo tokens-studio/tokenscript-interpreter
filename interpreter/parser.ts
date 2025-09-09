83d3cd3d230c154596c9d6c6284f307eb70306f7
Florian Schroedl <flo.schroedl@gmail.com>
2025-09-09 14:43:20 +0200

    Cleanup

--- interpreter/parser.ts
+++ interpreter/parser.ts
@@ -116,25 +116,20 @@ export class Parser {
   }
 
   private assignVariable(): AssignNode {
-    // consume 'variable'
     const token = this.eat(TokenType.RESERVED_KEYWORD);
 
-    // get variable name
     const varNameToken = this.eat(TokenType.STRING);
     const varName = new IdentifierNode(varNameToken);
     this.eat(TokenType.COLON);
 
-    // get type declaration
     const typeDecl = this.typeDeclaration();
 
-    // handle assignment if present
     if (this.currentToken.type === TokenType.ASSIGN) {
       this.eat(TokenType.ASSIGN);
       const assignmentExpr = this.listExpr();
       return new AssignNode(varName, typeDecl, assignmentExpr, token);
     }
 
-    // No assignment, just declaration
     return new AssignNode(varName, typeDecl, null, token);
   }
 
