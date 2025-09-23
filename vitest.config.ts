dd15fb27ab53693c2205e353b94d3e44ee13afc8
Florian Schroedl <flo.schroedl@gmail.com>
2025-09-23 10:37:10 +0200

    Fix ci including e2e

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    exclude: ["**/node_modules/**", "**/dist/**", "**/e2e/**", "examples/**/tests/e2e/**"],
   },
   resolve: {
     alias: {
