2238b30a6f3c28b1df6171f3c5c1a8b7954276cd
Florian Schroedl <flo.schroedl@gmail.com>
2025-09-24 20:49:27 +0200

    Add label

--- examples/web-repl/src/components/EditorModeTitle.tsx
+++ examples/web-repl/src/components/EditorModeTitle.tsx
@@ -16,21 +16,24 @@ function EditorModeTitle({
 }: EditorModeTitleProps) {
   if (inputMode && onInputModeChange) {
     return (
-      <div className="relative flex items-center h-full hover:bg-gray-100 border-r border-solid border-gray-200">
-        <DefaultShellTitle className="px-0">
-          <select
-            value={inputMode}
-            onChange={(e) => onInputModeChange(e.target.value as "tokenscript" | "json")}
-            className="h-full appearance-none bg-transparent border-none outline-none truncate cursor-pointer px-3 pr-10"
-            data-testid={testId || "editor-mode-dropdown"}
-          >
-            <option value="tokenscript">Tokenscript</option>
-            <option value="json">JSON</option>
-          </select>
-          <span className="pointer-events-none absolute right-1 pr-1">
-            <ArrowDown />
-          </span>
-        </DefaultShellTitle>
+      <div className="flex h-full items-center">
+        <DefaultShellTitle>Input</DefaultShellTitle>
+        <div className="relative flex items-center h-full hover:bg-gray-100 border-l border-r border-solid border-gray-200">
+          <DefaultShellTitle className="px-0">
+            <select
+              value={inputMode}
+              onChange={(e) => onInputModeChange(e.target.value as "tokenscript" | "json")}
+              className="h-full appearance-none font-bold bg-transparent border-none outline-none truncate cursor-pointer px-3 pr-10"
+              data-testid={testId || "editor-mode-dropdown"}
+            >
+              <option value="tokenscript">Tokenscript</option>
+              <option value="json">JSON</option>
+            </select>
+            <span className="pointer-events-none absolute right-1 pr-1">
+              <ArrowDown />
+            </span>
+          </DefaultShellTitle>
+        </div>
       </div>
     );
   }
