dd1b524cdbb2a027864a4a14267ec7dee11fec03
Florian Schroedl <flo.schroedl@gmail.com>
2025-09-30 14:15:52 +0200

    Share config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,41 +1,40 @@
 import { defineConfig } from "tsup";
 
+const sharedConfig = {
+  dts: true,
+  sourcemap: true,
+  treeshake: true,
+  tsconfig: "tsconfig.build.json",
+  silent: true,
+};
+
 export default defineConfig([
   // Library build
   {
     entry: ["src/lib/index.ts"],
     format: ["esm", "cjs"],
-    dts: true,
-    sourcemap: true,
     clean: true,
     outDir: "dist/lib",
     external: ["node:fs", "node:path", "node:url"],
-    treeshake: true,
-    tsconfig: "tsconfig.build.json",
+    ...sharedConfig,
   },
   // CLI build
   {
     entry: ["src/cli.ts"],
     format: ["esm"],
-    dts: true,
-    sourcemap: true,
     outDir: "dist",
     external: ["yauzl", "chalk", "commander", "readline-sync"],
-    treeshake: true,
-    tsconfig: "tsconfig.build.json",
     banner: {
       js: "#!/usr/bin/env node",
     },
+    ...sharedConfig,
   },
   // Supporting files needed by CLI
   {
     entry: ["src/compliance-suite.ts", "src/tokenset-processor.ts", "src/types.ts"],
     format: ["esm"],
-    dts: true,
-    sourcemap: true,
     outDir: "dist",
     external: ["node:fs", "node:path", "chalk"],
-    treeshake: true,
-    tsconfig: "tsconfig.build.json",
+    ...sharedConfig,
   },
 ]);
