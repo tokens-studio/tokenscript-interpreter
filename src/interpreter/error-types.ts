d1c80f34bc79095e69a079b961e609483143caa4
Florian Schroedl <flo.schroedl@gmail.com>
2025-10-16 16:03:34 +0200

    Remove bottle file

--- src/interpreter/error-types.ts
+++
@@ -1,7 +0,0 @@
-import type { ColorManagerErrorType } from "./config/managers/color/errors";
-import type { UnitManagerErrorType } from "./config/managers/unit/errors";
-
-export type InterpreterErrorType = ColorManagerErrorType | UnitManagerErrorType;
-
-export { ColorManagerError, type ColorManagerErrorType } from "./config/managers/color/errors";
-export { UnitManagerError, type UnitManagerErrorType } from "./config/managers/unit/errors";
